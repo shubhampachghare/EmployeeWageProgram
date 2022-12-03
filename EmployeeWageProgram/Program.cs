39d2b5578823da4be5152973ff8940f8a20df980
shubhampachghare <shubhampachghare@gmail.com>
2022-12-03 22:57:38 +0530

    [Shubham]- Welcome message

---
+++ EmployeeWageProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+        }
+    }
+
+}
